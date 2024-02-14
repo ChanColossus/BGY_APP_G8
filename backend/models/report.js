08e777897a6f40873fd3e37c4f78df115d955d17
ChanColossus <christianbenedict.quintero@tup.edu.ph>
2024-02-14 10:34:21 +0800

    Report Folders created

---
+++ backend/models/report.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define schema for Disaster Report
+const DisasterReportSchema = new Schema({
+    date: {
+        type: Date,
+        required: true
+    },
+    disaster: {
+        type: Schema.Types.ObjectId,
+        ref: 'Disaster',
+        required: true
+    },
+    area: {
+        type: Schema.Types.ObjectId,
+        ref: 'Area',
+        required: true
+    },
+    affectedPersons: {
+        type: Number,
+        required: true
+    }
+});
+
+// Create model for Disaster Report
+const DisasterReport = mongoose.model('DisasterReport', DisasterReportSchema);
+
+module.exports = DisasterReport;
